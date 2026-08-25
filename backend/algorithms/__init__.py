084104a07597bf2337618bfa22be5315f1948540
guxvr <guhxe32@gmail.com>
2026-08-25 11:15:45 -0300

    feat: adiciona implementacao do algoritmo de Dijkstra

---
+++ backend/algorithms/__init__.py
@@ -0,0 +1,6 @@
+"""Algoritmos de caminho minimo usados pelo simulador."""
+
+from backend.algorithms.dijkstra import dijkstra
+from backend.algorithms.result import RouteResult
+
+__all__ = ["RouteResult", "dijkstra"]
